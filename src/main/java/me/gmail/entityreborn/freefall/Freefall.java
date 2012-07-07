bb18cd1b1dde38727d9644ae418651c3ea5ae75a
EntityReborn <entityreborn@gmail.com>
2012-07-06 17:41:51 -0700

    Added config options.

--- src/main/java/me/gmail/entityreborn/freefall/Freefall.java
+++ src/main/java/me/gmail/entityreborn/freefall/Freefall.java
@@ -11,6 +11,9 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class Freefall extends JavaPlugin implements Listener {
     public void onEnable() {
         getServer().getPluginManager().registerEvents(this, this);
+        
+        getConfig().options().copyDefaults(true);
+        saveConfig();
     }
 
     @EventHandler
@@ -19,8 +22,40 @@ public class Freefall extends JavaPlugin implements Listener {
             if(event.getCause() == DamageCause.FALL) {
                 Player p = (Player)event.getEntity();
                 
-                if(p.isSneaking() || p.hasMetadata("freefall.protect")) {
-                    event.setCancelled(true);
+                if(p.hasPermission("freefall.protect")) {
+                    int distance;
+                    
+                    String consolemsg;
+                    String playermsg;
+                    
+                    if(p.isSneaking()) {
+                        distance = getConfig().getInt("sneak-max-distance");
+                        consolemsg = getConfig().getString("sneak-console-msg");
+                        playermsg = getConfig().getString("sneak-player-msg");
+                    } else {
+                        distance = getConfig().getInt("stand-max-distance");
+                        consolemsg = getConfig().getString("stand-console-msg");
+                        playermsg = getConfig().getString("stand-player-msg");
+                    }
+                    
+                    if (distance == 0 || p.getFallDistance() <= distance) {
+                        if(!consolemsg.isEmpty()){
+                            getServer().getConsoleSender()
+                                .sendMessage(consolemsg
+                                    .replace("%NAME%", p.getName())
+                                    .replace("%DIST%", 
+                                        String.valueOf((int)p.getFallDistance())));
+                        }
+                        
+                        if(!playermsg.isEmpty()){
+                            p.sendMessage(playermsg
+                                .replace("%NAME%", p.getName())
+                                .replace("%DIST%", 
+                                    String.valueOf((int)p.getFallDistance())));
+                        }
+                    
+                        event.setCancelled(true);
+                    }
                 }
             }
         }
