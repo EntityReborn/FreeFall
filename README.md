f8c372ba5a2567b53321f7be32526f981de2b69a
EntityReborn <entityreborn@gmail.com>
2012-07-13 19:46:33 -0700

    Switched from a height based system to a calculated damage system. Added reload and version commands.

--- README.md
+++ README.md
@@ -2,62 +2,79 @@
 
 ##Permissions based freefall for minecraft players##
 
-Allow users to fall a configurable distance without damage!
-
----
+  Allow users to fall with configurable damage!
 
 ##Installation##
 
-Drop into your /plugins/ folder, run the server once to generate the config file, then modify to taste.
+  Drop into your /plugins/ folder, run the server once to generate the config file, then modify config to taste.
 
----
+##Commands##
+ 
+  The plugin is fairly self sufficient, but you can use the following commands:
+  
+  * `/freefall version`: find out what version of freefall you are running.
+  * `/freefall reload`: reload the config file.
 
 ##Configuration##
 
-Several options are available:
+  Several options are available:
 
-###Distance###
+  * Damage Calculation
 
-Set any of these to 0 for an infinite fall distance.
+    Set any of these to `0` for an infinite fall distance without damage.
 
-* `stand-max-distance`: maximum distance a user can freefall while standing.
-* `sneak-max-distance`: maximum distance while sneaking (crouching).
+    Basically, any simple calculation can be done here, 'damage' would be replaced
+with the default damage if this weren't handled, 'distance' would be the distance
+fallen.
 
-###Messages###
+    * `stand-calculation`: math expression of which the result will be the amount of damage when standing.
+    * `sneak-calculation`: same as above while sneaking.
 
-Set the message to "" to cancel sending a message.
-`%NAME%` here will be replaced with the user's name, and `%DIST%` will be replaced with the distance fell.
-These messages will only be sent if the user is within the confines of the distance parameters mentioned above.
+    If the result is less than zero, it will be interpreted as a damage of zero.
 
-Be sure to always double quote these strings, and escape any double quotes used in the message.
-(IE, `"%NAME% fell %DIST% blocks while standing, but didn't get hurt!"`)
+  * Messages
 
-* `stand-console-msg`: message to be sent to the console when standing.
-* `sneak-console-msg`: message to be sent to the console when sneaking.
-* `stand-player-msg`: message to be sent to the player when standing.
-* `sneak-player-msg`: message to be sent to the player when sneaking.
+    Set the message to `""` to cancel sending a message.
 
----
+    `%NAME%` here will be replaced with the user's name, and `%DIST%` will be replaced with the distance fell. These messages will only be sent if the user is within the confines of the distance parameters mentioned above.
+
+    Be sure to always double quote these strings, and escape any double quotes used in the message. (IE, `"%NAME% fell %DIST% blocks while standing, but didn't get hurt!"`)
+
+    Messages for when not receiving damage:
+
+    * `stand-console-msg`: message to be sent to the console when standing.
+    * `sneak-console-msg`: message to be sent to the console when sneaking.
+    * `stand-player-msg`: message to be sent to the player when standing.
+    * `sneak-player-msg`: message to be sent to the player when sneaking.
+
+    Messages for when receiving damage. (These also have a `%DAMG%` keyword)
+
+    * `stand-console-msg-damaged`: message to be sent to the console when standing and receiving damage.
+    * `sneak-console-msg-damaged`: message to be sent to the console when sneaking and receiving damage.
+    * `stand-player-msg-damaged`: message to be sent to the player when standing and receiving damage.
+    * `sneak-player-msg-damaged`: message to be sent to the player when sneaking and receiving damage.
 
 ##Permissions##
 
-###Access###
-Give users or groups these permissions to affect their freefall.
+  Permissions affect how the plugin deals with fall damage.
 
-* `freefall.sneak`: allow the user to painlessly freefall the `sneak-max-distance` distance while sneaking.
-* `freefall.stand`: allow the user to painlessly freefall the `stand-max-distance` distance while standing.
+  * Access
 
-###Bypasses###
-You might want to give some players the ability to bypass the limit, allowing them to freefall any distance.
+    Give users or groups these permissions to allow the plugin to affect their freefall.
 
-* `freefall.sneak.bypass`: bypass sneak limits
-* `freefall.stand.bypass`: bypass stand limits
+    * `freefall.sneak`: allow the user to have their damage re-calculated while sneaking.
+    * `freefall.stand`: allow the user to have their damage re-calculated while standing.
 
----
+  * Bypasses
+
+    You might want to give some players the ability to bypass the limit, allowing them to freefall any distance.
+
+    * `freefall.sneak.bypass`: bypass sneak limits
+    * `freefall.stand.bypass`: bypass stand limits
 
 ##Credits##
 
-* Me (`EntityReborn` aka `__import__`) - Code / Implementation
-* Malvagio87 from 10hearts.com - Original idea
+  * Me (`EntityReborn` aka `__import__`) - Code / Implementation
+  * Malvagio87 from 10hearts.com minecraft community - Original idea
 
 You can usually find me in `#CommandHelper` on `irc.esper.net`, usually with nick `__import__` or `Masamune`.
--- src/main/java/me/gmail/entityreborn/freefall/Freefall.java
+++ src/main/java/me/gmail/entityreborn/freefall/Freefall.java
@@ -1,5 +1,7 @@
 package me.gmail.entityreborn.freefall;
 
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -7,6 +9,10 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.matheclipse.parser.client.eval.DoubleEvaluator;
+import org.matheclipse.parser.client.eval.DoubleVariable;
+import org.matheclipse.parser.client.eval.IDoubleValue;
+
 
 public class Freefall extends JavaPlugin implements Listener {
     public void onEnable() {
@@ -15,18 +21,43 @@ public class Freefall extends JavaPlugin implements Listener {
         getConfig().options().copyDefaults(true);
         saveConfig();
     }
-
+    
+    public boolean onCommand(CommandSender sender, Command command, String commandLabel, String[] args) {
+        String cmdname = command.getName().toLowerCase();
+        
+        if ((cmdname.equals("freefall")) && (args.length > 0)) {
+            String cmd = args[0].toLowerCase();
+            
+            if (cmd.equals("reload")) {
+                if (sender.isOp() || sender.hasPermission("freefall.reload")) {
+                    reloadConfig();
+                    sender.sendMessage("FreeFall reloaded");
+                    return true;
+                }
+            } else if (cmd.equals("version")) {
+                if (sender.isOp() || sender.hasPermission("freefall.reload")) {
+                    sender.sendMessage("FreeFall " + getDescription().getVersion());
+                    return true;
+                }
+            }
+        }
+        
+        return false;
+    }
+    
     @EventHandler
     public void onEntityDamage(EntityDamageEvent event) {
         if (event.getEntityType() == EntityType.PLAYER) {
             if(event.getCause() == DamageCause.FALL) {
                 Player p = (Player)event.getEntity();
                 
-                
-                int distance;
+                boolean bypass = false;
                 
                 String consolemsg;
                 String playermsg;
+                String consolemsgdmgd;
+                String playermsgdmgd;
+                String eval;
                 
                 if(p.isSneaking()) {
                     if(!p.hasPermission("freefall.sneak")) {
@@ -34,45 +65,81 @@ public class Freefall extends JavaPlugin implements Listener {
                     }
                     
                     if(p.hasPermission("freefall.sneak.bypass")) {
-                        distance = 0;
-                    } else {
-                        distance = getConfig().getInt("sneak-max-distance");
+                        bypass = true;
                     }
                     
+                    eval = getConfig().getString("sneak-calculation");
                     consolemsg = getConfig().getString("sneak-console-msg");
                     playermsg = getConfig().getString("sneak-player-msg");
+                    consolemsgdmgd = getConfig().getString("sneak-console-msg-damaged");
+                    playermsgdmgd = getConfig().getString("sneak-player-msg-damaged");
                 } else {
                     if(!p.hasPermission("freefall.stand")) {
                         return;
                     }
                     
                     if(p.hasPermission("freefall.stand.bypass")) {
-                        distance = 0;
-                    } else {
-                        distance = getConfig().getInt("stand-max-distance");
-                    }
+                        bypass = true;
+                    } 
                     
+                    eval = getConfig().getString("stand-calculation");
                     consolemsg = getConfig().getString("stand-console-msg");
                     playermsg = getConfig().getString("stand-player-msg");
+                    consolemsgdmgd = getConfig().getString("stand-console-msg-damaged");
+                    playermsgdmgd = getConfig().getString("stand-player-msg-damaged");
                 }
                 
-                if (distance == 0 || p.getFallDistance() <= distance) {
+                double damage = 0;
+                int falldistance = (int)p.getFallDistance() + 3;
+                
+                if (!bypass) {
+                    IDoubleValue fallen = new DoubleVariable(falldistance);
+                    IDoubleValue dmg = new DoubleVariable((int)event.getDamage());
+                    DoubleEvaluator engine = new DoubleEvaluator();
+                    engine.defineVariable("distance", fallen);
+                    engine.defineVariable("damage", dmg);
+                    damage = engine.evaluate(eval);
+                }
+                
+                if (bypass || (int)damage == 0) {
                     if(!consolemsg.isEmpty()){
                         getServer().getConsoleSender()
                             .sendMessage(consolemsg
                                 .replace("%NAME%", p.getName())
                                 .replace("%DIST%", 
-                                    String.valueOf((int)p.getFallDistance())));
+                                    String.valueOf(falldistance)));
                     }
                     
                     if(!playermsg.isEmpty()){
                         p.sendMessage(playermsg
                             .replace("%NAME%", p.getName())
                             .replace("%DIST%", 
-                                String.valueOf((int)p.getFallDistance())));
+                                String.valueOf(falldistance)));
                     }
                 
                     event.setCancelled(true);
+                } else {
+                    if ((damage) > 0) {
+                        if(!consolemsgdmgd.isEmpty()){
+                            getServer().getConsoleSender()
+                                .sendMessage(consolemsgdmgd
+                                    .replace("%NAME%", p.getName())
+                                    .replace("%DAMG%", String.valueOf((int)damage))
+                                    .replace("%DIST%", 
+                                        String.valueOf(falldistance)));
+                        }
+                        
+                        if(!playermsgdmgd.isEmpty()){
+                            p.sendMessage(playermsgdmgd
+                                .replace("%NAME%", p.getName())
+                                .replace("%DAMG%", String.valueOf((int)damage))    
+                                .replace("%DIST%", 
+                                    String.valueOf(falldistance)));
+                        }
+                        
+                        event.setDamage((int) damage);
+                    }
+                        
                 }
             }
         }
